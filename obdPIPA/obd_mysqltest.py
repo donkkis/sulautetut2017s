f56afefe7b1072cd05dfb0fed0b730c98fbce0ce
donkkis <panu.aho@gmail.com>
2017-11-15 23:18:48 +0200

    Implemented GPS logging with GPSD/gps3 libraries

--- obdPIPA/obd_mysqltest.py
+++ obdPIPA/obd_mysqltest.py
@@ -1,13 +1,19 @@
-#PA 11.11.2017
+#PA 2017
 #First test of python OBD library + mysql server functionality in Raspberry
-
-#TODO: Exit from loop when serial connection is lost or "something else" happens
 
 import obd
 import datetime
 import mysql.connector
 from time import sleep
+from subprocess import call
+from gps3.agps3threaded import AGPS3mechanism
 
+#----------Initialize GPS receiver-----------
+agps_thread = AGPS3mechanism()
+agps_thread.stream_data()
+agps_thread.run_thread()
+
+#---------Initialize ELM327------------------
 #First param is the RFCOMM port (e.g. '/dev/rfcomm0') and second is baud rate
 #None is the default and performs automatic lookup of serial connection (bluetooth/usb)
 #9600 works as the baud rate at least for obdSIM
@@ -23,7 +29,7 @@ obd.commands.RPM,
 obd.commands.ENGINE_LOAD,
 obd.commands.SPEED]
 
-#initialize db connection
+#initialize db connection (potentially unsafe!)
 usr = 'panu'
 pw = 'panu'
 db = 'obdlogger'
@@ -33,26 +39,27 @@ cur = cnx.cursor()
 
 db_query = ("INSERT into Entry (DeviceID, RPM, Calc_load, Speed, GPS_Lat, GPS_Long) VALUES ({0}, {1}, {2}, {3}, {4}, {5})")
 
+#initialize for conditional stopping
 exit_count = 0
 
 #set query interval globally
 interval = 1
 
 #shutdown latency in seconds
-ttl = 30
+ttl = 10
 
 #-----------MAIN LOOP-----------------
-#Need to be changed to a while with conditional exit (connection lost/ignition power off)
+#Aqcuire data from target with conditional exit -> connection lost/ignition power off
 while exit_count < (ttl/interval):
   r = []
   c_status = c.status()
-  
+
   #check exit conditions
   #-ELM327 connection down
-  #-Engine not running
-  if c_status == "Not Connected" or (rpm.value.magnitude == 0 or rpm.value == "None"):
+  #-TODO Engine not running
+  if c_status == "Not Connected":
     exit_count += 1
-    print("Exit count ", exit_count)
+    print("Waiting " + str(ttl/interval -  exit_count) + " seconds for reconnection...")
 
   if c_status == "Car Connected":
 
@@ -65,16 +72,13 @@ while exit_count < (ttl/interval):
       else:
         r.append(-99)
 
-    #TODO implement geolocation from actual service, these are just random values for simulation
-    #GPS_Lat 
-    r.append(34.555555)
-
-    #GPS_Long
-    r.append(84.555555)
-  
     #debug
     print(r)
-  
+
+    #add GPS coordinates
+    r.append(agps_thread.data_stream.lat)
+    r.append(agps_thread.data_stream.lon)
+
     #Commit ECU query results to db
     #GPS is only for simulation at this point
     cur.execute(db_query.format(1, r[0], r[1], r[2], r[3], r[4]))
@@ -82,4 +86,8 @@ while exit_count < (ttl/interval):
  
   sleep(interval)
 
-print("Main loop stopped")
+print("No connection, stopping main loop.\nMain loop stopped")
+
+#Will power off the system
+#Keep commented for developement
+#call("sudo shutdown -h now", shell=True)
